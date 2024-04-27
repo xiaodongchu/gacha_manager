268a734a3b688a23899298812b71e66d79cc654f
xiaodongchu <2549369356@qq.com>
2024-04-28 02:06:48 +0800

    支持原神4.6

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # 修改为自己的游戏绝对路径
 genshin_game_path = ("D:/software/games/Genshin Impact/Genshin Impact Game/"
-                     "YuanShen_Data/webCaches/2.22.0.0/Cache/Cache_Data/data_2")
+                     "YuanShen_Data/webCaches/2.24.0.0/Cache/Cache_Data/data_2")
 star_rail_game_path = ("D:/software/games/Star Rail/Game/"
                        "StarRail_Data/webCaches/2.22.0.0/Cache/Cache_Data/data_2")

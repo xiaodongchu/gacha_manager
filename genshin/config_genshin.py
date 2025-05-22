45d045e5240ee71b4c9d4c16601f3449d7a65fd8
xiaodongchu <2549369356@qq.com>
2025-05-22 23:53:37 +0800

    星铁3.3

--- genshin/config_genshin.py
+++ genshin/config_genshin.py
@@ -2,6 +2,6 @@ import os
 
 # 修改为自己的游戏绝对路径，注意版本号(如2.24.0.0)取最新(最大)的
 # 原神路径
-genshin_game_path = "D:/software/games/mihoyo/Genshin Impact Game/YuanShen_Data/webCaches/2.34.0.0/Cache/Cache_Data/data_2"
+genshin_game_path = "D:/software/games/mihoyo/Genshin Impact Game/YuanShen_Data/webCaches/2.37.0.0/Cache/Cache_Data/data_2"
 # 当前路径，无需修改
 base_dir = os.path.dirname(__file__)
